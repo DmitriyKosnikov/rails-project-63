edbdb8658603d2189c9610eebf6372af4ea0103a
DmitriyKosnikov <kosnikov.dm@gmail.com>
2024-12-10 12:47:53 +0300

    step 4

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -8,4 +8,13 @@ module HexletCode
   # Your code goes here...
 
   autoload(:Tag, "hexlet_code/tag.rb")
+
+  def self.form_for(entity, **options)
+    action = options.delete(:url) || "#"
+    method = "post"
+
+    prioritized_options = { action: action, method: method }
+
+    Tag.build("form", **prioritized_options.merge(options)) { yield if block_given? }
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -2,6 +2,8 @@
 
 require "test_helper"
 
+User = Struct.new("User", :name, :job, keyword_init: true)
+
 class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
@@ -9,5 +11,35 @@ class TestHexletCode < Minitest::Test
 
   def test_it_does_something_useful
     assert false
+  end
+
+  def test_form_for_only_user
+    expected_value = "# <form action=\"#\" method=\"post\"></form>"
+
+    user = User.new
+
+    formed = HexletCode.form_for user do |f|
+    end
+
+    assert(formed, expected_value)
+  end
+
+  def test_form_for_with_options
+    expected_value = "<form action=\"#\" method=\"post\" class=\"hexlet-form\"></form>"
+
+    user = User.new
+
+    formed = HexletCode.form_for user, class: "hexlet-form" do |f|
+    end
+
+    assert(formed, expected_value)
+  end
+
+  def test_form_for_with_block
+    user = User.new
+
+    result = HexletCode.form_for(user) { "Inner Content" }
+    expected = '<form action="#" method="post">Inner Content</form>'
+    assert_equal expected, result, "Form should include content from the block"
   end
 end
